372d42a995b2248eb95f5e6482d8479c7ad8466c
jamujica6 <jamujica6@gmail.com>
2024-08-11 21:34:41 -0400

    aleV2.1

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -6,7 +6,6 @@ export const metadata = {
 import Hero from '@/components/hero'
 import Features from '@/components/features'
 import Newsletter from '@/components/newsletter'
-// import Zigzag from '@/components/zigzag'
 import Testimonials from '@/components/testimonials'
 
 export default function Home() {

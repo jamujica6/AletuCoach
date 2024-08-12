83050211c6a37701cf0d93da67e823c42bf08d4d
jamujica6 <jamujica6@gmail.com>
2024-08-12 11:05:55 -0400

    AletuCoch3.1

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -30,14 +30,15 @@ export default function DefaultLayout({
 
 
 <div style={{ 
-
+  background: 'white',
   backgroundImage: 'url(/images/aleunpocodemi.jpg)',
 //  'url("../../videos/fondo.mp4")' , 
 //  'url("https://images.unsplash.com/photo-1473116763249-2faaef81ccda?q=80&w=1796&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
   // filter: 'brightness(90%) contrast(120%)', 
   // backgroundAttachment: 'fixed',
-  backgroundSize: 'cover',
+  backgroundSize: 'contain',
   backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
   width: '100vw',
   height: '100vh',
   overflow: 'auto',

3d775ebaab09d2674d1c16ca2d135e107cb1e500
jamujica6 <jamujica6@gmail.com>
2024-08-12 11:14:45 -0400

    AletuCoch3.2

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -72,7 +72,7 @@ export default function Footer() {
                 </Link>
               </li>
               <li className="ml-4">
-                <Link href="/" className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out" aria-label="Instagram">
+                <Link href="https://www.instagram.com/ale.tucoach/" className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out" aria-label="Instagram">
                   <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                     <circle cx="20.145" cy="11.892" r="1" />
                     <path d="M16 20c-2.206 0-4-1.794-4-4s1.794-4 4-4 4 1.794 4 4-1.794 4-4 4zm0-6c-1.103 0-2 .897-2 2s.897 2 2 2 2-.897 2-2-.897-2-2-2z" />
